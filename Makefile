ea95a4f9ca66d180210192127b92fb12851e89c4
Fernando Figueiredo <fergueiredo@gmail.com>
2024-10-02 21:04:38 +0100

    chore: First commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+build:
+	./mvnw clean package
+run:
+	./mvnw spring-boot:run
---
+++ src/main/java/io/excon/ExConApplication.java
@@ -0,0 +1,13 @@
+package io.excon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExConApplication.class, args);
+	}
+
+}
